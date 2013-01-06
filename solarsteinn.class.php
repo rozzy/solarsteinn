74b162c12e19a3dc8c5acc9ce47aad8cd228d64f
Nikita Nikitin <berozzy@gmail.com>
2013-01-07 00:14:05 +0200

    Solved locale-troubles

--- solarsteinn.class.php
+++ solarsteinn.class.php
@@ -7,7 +7,7 @@
 
 		public function __construct() {
 			if (self::use_utf) mb_internal_encoding('UTF-8');
-			setlocale(LC_ALL, (isset(self::lang) && self::lang != '' ? self::lang : 'en_EN').(self::use_utf ? '.UTF-8' : '')); 
+			setlocale(LC_ALL, (self::lang != '' ? self::lang : 'en_EN').(self::use_utf ? '.UTF-8' : ''));
 		}
 
 		public static function compile ($time, $data, $outputTemplate = self::default_output_template) {
